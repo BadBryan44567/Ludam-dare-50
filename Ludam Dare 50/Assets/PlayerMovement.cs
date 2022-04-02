423549af2ee7f6bdd6fd06a2dd9bec379b8896e8
BadBryan44567 <71113321+BadBryan44567@users.noreply.github.com>
2022-04-02 09:58:29 +0530

    Added Player Controller and Pick Up System

---
+++ Ludam Dare 50/Assets/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public CharacterController controller;
+	public float speed = 12f;
+
+	Vector3 velocity;
+	public float gravity = -9.81f;
+	public Transform groundCheck;
+	public float groundDistnace;
+	public LayerMask groundMask;
+	bool isGrounded;
+
+	public float jumpHeight = 3;
+
+	// Update is called once per frame
+	void Update()
+	{
+		isGrounded = Physics.CheckSphere(groundCheck.position, groundDistnace, groundMask);
+
+		if (isGrounded && velocity.y < 0)
+		{
+			velocity.y = -2f;
+		}
+
+		float xInput = Input.GetAxis("Horizontal");
+		float zInput = Input.GetAxis("Vertical");
+
+		Vector3 move = transform.right * xInput + transform.forward * zInput;
+		controller.Move(move * speed * Time.deltaTime);
+
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+		{
+			velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+		}
+
+		velocity.y += gravity * Time.deltaTime;
+		controller.Move(velocity * Time.deltaTime);
+
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Pickup Item")
+		{
+			Destroy(other.gameObject);
+		}
+	}
+}
